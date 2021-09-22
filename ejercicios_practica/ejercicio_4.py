f7d0e877ebeb336917f741c21a1b66947f06ca3f
SteffiGrim <svgrimaldi@gmail.com>
2021-09-22 00:10:28 -0300

    tarea finalizada. Disculpen, le hice cambios para hacerle una broma a mi amiga

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -20,6 +20,35 @@ palabra_2 = str(input())
 print('Ingrese palabra 3:')
 palabra_3 = str(input())
 
+print('Ingrese palabra 4:')
+palabra_4 = str(input())
+
+print('Ingrese palabra 5:')
+palabra_5 = str(input())
+
+print('Ingrese palabra 6:')
+palabra_6 = str(input())
+
+print('Ingrese palabra 7:')
+palabra_7 = str(input())
+
+print('Ingrese palabra 8:')
+palabra_8 = str(input())
+
+print('Ingrese palabra 9:')
+palabra_9 = str(input())
+
+print('Ingrese palabra 10:')
+palabra_10 = str(input())
+
+print('Ingrese palabra 11:')
+palabra_11 = str(input())
+
+print('Ingrese palabra 12:')
+palabra_12 = str(input()) 
+
 # De cada palabra debe tomar la primera letra y armar el acrónimo
 # Ejemplo: Alumbrado, barrido y limpieza --> ABL
 # Imprimir el resultado en pantalla
+
+print (palabra_1[0] + palabra_2[0] + palabra_3[0] + palabra_4[0] + palabra_5[0] + palabra_6[0] + palabra_7[0] + palabra_8[0] + palabra_9[0] + palabra_10[0] + palabra_11[0] + palabra_12[0])
